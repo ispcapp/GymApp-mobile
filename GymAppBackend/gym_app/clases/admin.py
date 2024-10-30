40d8e2b960786aaae0fac7758d9b0b7ba1fa2b1d
Laurix86 <grossoanalaura@gmail.com>
2024-10-29 21:15:13 -0300

    superuser

--- GymAppBackend/gym_app/clases/admin.py
+++ GymAppBackend/gym_app/clases/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, Actividad, Clase, Reserva, Categoria, ActividadCategoria
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Actividad)
+admin.site.register(Clase)
+admin.site.register(Reserva)
+admin.site.register(Categoria)
+admin.site.register(ActividadCategoria)
