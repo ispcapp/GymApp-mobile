1dd84b56be73a1a1ddb1eb840a387f2228cbac10
lanusroots <lanusroots@gmail.com>
2024-11-05 21:40:47 -0300

    Frontend actualizado

--- gymApp/app/src/main/java/com/ispcapp/gymapp/LoginActivity.java
+++ gymApp/app/src/main/java/com/ispcapp/gymapp/LoginActivity.java
@@ -6,17 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     ImageView btnBack;
     Button btnLogin;
+    TextView tv_forgot_pass;
 
     @SuppressLint({"MissingInflatedId"})
     @Override
@@ -44,6 +47,21 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        // Manejador de Fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        tv_forgot_pass = findViewById(R.id.tv_forgot_pass);
+        tv_forgot_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, ForgotPasswordFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
             }
         });
     }
