6c4aa745e377c11d5ff6738a886a7ad759a5bc8e
Carlos-Ferri-Del-Castillo <92340552+Carlos-Ferri-Del-Castillo@users.noreply.github.com>
2024-11-07 02:45:10 -0300

    Revert "Carlos ferri del castillo"

--- gymApp/app/build.gradle.kts
+++ gymApp/app/build.gradle.kts
@@ -3,17 +3,16 @@ plugins {
 }
 
 android {
-    namespace = "com.ispcapp.gymapp"
+    namespace = "com.ispcapp.myapplication"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.ispcapp.gymapp"
+        applicationId = "com.ispcapp.myapplication"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
-       // manifestPlaceholders = [auth0Domain: "@string/com_auth0_domain", auth0Scheme: "demo"]
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -38,13 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.circleimageview)
-    implementation(libs.navigation.fragment)
-    implementation(libs.navigation.ui)
-    implementation(libs.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.retrofit)
-    implementation(libs.retrofit2.converter.gson)
 }
