764f76b60c08d8cb977fbf4d7bceaac66ed10200
Carlos-Ferri-Del-Castillo <carlosferridelcastillo@gmail.com>
2024-11-07 02:23:46 -0300

    agregando pantalla ajustes-contraseñas

--- gymApp/app/src/main/java/com/ispcapp/gymapp/SettingsFragment.java
+++ gymApp/app/src/main/java/com/ispcapp/gymapp/SettingsFragment.java
@@ -3,10 +3,13 @@ package com.ispcapp.gymapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class SettingsFragment extends Fragment {
@@ -18,7 +21,24 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        TextView tv_password_settings, tv_delete_account_settings;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View componentFragment = inflater.inflate(R.layout.fragment_settings, container, false);
+        // Manejador de Fragments
+        FragmentManager fragmentManager = getChildFragmentManager();
+
+        tv_password_settings = componentFragment.findViewById(R.id.tv_password_settings);
+        tv_password_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fcv_settings, ChangePasswordFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+        return componentFragment;
     }
 }
